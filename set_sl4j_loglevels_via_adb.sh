bb647afc515ee9c8f3d89149e0a25c57ee90c71f
Johannes Zweng <john@zweng.at>
2017-10-22 11:47:33 +0200

    shell script for setting sl4j loglevels via adb

---
+++ set_sl4j_loglevels_via_adb.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#
+# Set the loglevel for the classes using SL4J via adb
+# Call this script with a log-level as first argument:
+#  > ./set_sl4j_loglevels_via_adb.sh INFO
+#  > ./set_sl4j_loglevels_via_adb.sh DEBUG
+#  > ./set_sl4j_loglevels_via_adb.sh TRACE
+#
+adb shell setprop log.tag.EmvParser "$1"
+adb shell setprop log.tag.TrackUtils "$1"
+adb shell setprop log.tag.DataFactory "$1"
+adb shell setprop log.tag.AbstractByteBean "$1"
+adb shell setprop log.tag.EnumUtils "$1"
+adb shell setprop log.tag.ResponseUtils "$1"
+adb shell setprop log.tag.TrackUtils "$1"
+
